4459e28cc14403bc1effa852e5a2a2dfe6c27c8b
rookielxy <ryelovespython@gmail.com>
2018-12-26 21:29:07 +0800

    change test directory

--- main.cpp
+++ main.cpp
@@ -4,13 +4,14 @@
 int main(int argc, char *args[]) {
 	if (argc == 1)
 		return 0;
-	string tmStr(args[1]);
-	string inStr(args[2]);
+	string dirStr(args[1]);
+	string tmStr = dirStr + "/test.tm";
+	string inStr = dirStr + "/input.txt";
 
 	ifstream tm(tmStr, ios::in);
 	ifstream in(inStr, ios::in);
-	ofstream console("console.txt", ios::out);
-	ofstream result("result.txt", ios::out);
+	ofstream console(dirStr + "/console.txt", ios::out);
+	ofstream result(dirStr + "/result.txt", ios::out);
 
 	TuringMachine *turing = new TuringMachine(tm);
 	turing->simulate(in, console, result);
